2c3bf201b1ff2d69882458708d7a904bad921dd9
Romain Cognard <rcognard@mynetwk.biz>
2022-09-29 09:56:10 +0200

    Home Spinner Working

--- SaasOhMyFood.ps1
+++ SaasOhMyFood.ps1
@@ -1,3 +1,3 @@
 cd .\Sass
 
-sass --watch menu-style.scss:../styles/menu-style.css anim-style.scss:../styles/anim-style.css style.scss:../styles/style.css
+sass --watch menu-style.scss:../styles/menu-style.css style.scss:../styles/style.css
